c921ce38761736b7def4a1d632e3c7c73c6705ee
atsuki <csatsfapsfu2930@gmail.com>
2023-09-01 15:38:59 -0700

    get currency converstion

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-
+CURRENCY_API_KEY=os.getenv("CURRENCY_API_KEY")
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -15,3 +15,14 @@ intents.presences = False
 client = discord.Client(intents=intents)
 
 
+def get_currency_conversion(base_currency, target_currency):
+    url = f'https://v6.exchangerate-api.com/v6/{CURRENCY_API_KEY}/latest/{base_currency}'
+    response = requests.get(url)
+    data = response.json()
+    print(data['conversion_rates'][target_currency])
+    return data['conversion_rates'][target_currency]
+
+
+
+if __name__ == "__main__":
+    get_currency_conversion('CAD', 'JPY')
