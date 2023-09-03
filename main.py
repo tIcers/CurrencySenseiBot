015eb7378a407e2cf07e4cc104cf3d7561bdf72c
atsuki <csatsfapsfu2930@gmail.com>
2023-09-02 17:27:25 -0700

    implemented rate command

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import ssl
 import asyncio
 import aiohttp
 import certifi
+import requests
 from discord.ext import commands
 from const import CURRENCY_CHANNEL_ID, ECON_NEWS_CHANNEL_ID
 
@@ -14,11 +15,9 @@ os.environ['SSL_CERT_FILE'] = certifi.where()
 TOKEN = os.environ.get("TOKEN")
 CURRENCY_API_KEY = os.environ.get("CURRENCY_API_KEY")
 
-
 intents = discord.Intents.default()
-# client = commands.Bot(command_prefix='!', intents=intents)
 intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
 async def make_request():
@@ -27,52 +26,65 @@ async def make_request():
             print(await response.text())
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"We have logged in as {client.user.name}")
+    print(f"We have logged in as {bot.user.name}")
     await make_request()
 
 
-@client.event
+@bot.event
 async def on_message(message):
 
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.lower() == 'hello':
         await message.channel.send('Hey There!')
 
+    await bot.process_commands(message)
 
-# def get_currency_conversion(base_currency, target_currency):
-#     url = f'https://v6.exchangerate-api.com/v6/{CURRENCY_API_KEY}/latest/{base_currency}'
-#     response = requests.get(url)
-#     data = response.json()
-#     print(f"{target_currency} to {base_currency} is {data['conversion_rates'][target_currency]}")
-#     return data['conversion_rates'][target_currency]
-#
-#
-# async def check_currency_flucturations():
-#     channel_id = CURRENCY_CHANNEL_ID
-#     previous_rate = None
-#     base_currency = 'CAD'
-#     target_currency = 'JPY'
-#     threshold = 0.6
-#
-#     while True:
-#         rate = get_currency_conversion(base_currency, target_currency)
-#         if previous_rate is not None:
-#             if abs(rate - previous_rate) >= threshold:
-#                 channel = client.get_channel(channel_id)
-#                 await channel.send(f'Currency Fluctuation detected: {base_currency}/{target_currency} = {rate}')
-#
-#         previous_rate = rate
-#
-#         await asyncio.sleep(3600)
-#
-#
-# async def economic_calender_notification():
-#     pass
+
+@bot.command()
+async def rate(ctx, *args):
+    if not args:
+        await ctx.send("Please provide the base currency\n (e.g., !rate USD)")
+        return
+
+    base_currency = args[0].upper()
+    target_currency = 'JPY'
+    rate = get_currency_conversion(base_currency, target_currency)
+    await ctx.send(f'{base_currency.upper()} To JPY is {rate}')
+
+
+def get_currency_conversion(base_currency, target_currency):
+    url = f'https://v6.exchangerate-api.com/v6/{CURRENCY_API_KEY}/latest/{base_currency}'
+    response = requests.get(url)
+    data = response.json()
+    print(f"{target_currency} to {base_currency} is {data['conversion_rates'][target_currency]}")
+    return data['conversion_rates'][target_currency]
+
+
+async def check_currency_flucturations():
+    channel_id = CURRENCY_CHANNEL_ID
+    previous_rate = None
+    base_currency = 'CAD'
+    target_currency = 'JPY'
+    threshold = 0.6
+
+    while True:
+        rate = get_currency_conversion(base_currency, target_currency)
+        if previous_rate is not None:
+            if abs(rate - previous_rate) >= threshold:
+                channel = bot.get_channel(channel_id)
+                await channel.send(f'Currency Fluctuation detected: {base_currency}/{target_currency} = {rate}')
+
+        previous_rate = rate
+
+        await asyncio.sleep(3600)
+
+
+async def economic_calender_notification():
+    pass
 
 if __name__ == "__main__":
-    # get_currency_conversion('CAD', 'JPY')
-    client.run(TOKEN)
+    bot.run(TOKEN)
