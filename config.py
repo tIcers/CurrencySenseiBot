19c04136001d4611c919fcd7f27234c6e7cae542
atsuki <csatsfapsfu2930@gmail.com>
2024-01-07 17:41:25 -0800

    added headers

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 VANCOUVER_TIMEZONE = 'America/Vancouver'
 
+HEADERS = {
+        'User-Agent':
+	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:122.0) Gecko/20100101 Firefox/122.0'
+}
