36528b8b59a69e8f0e3cfcca7472811ca566fdb4
atsuki <csatsfapsfu2930@gmail.com>
2024-01-07 17:41:21 -0800

    updated const

--- const.py
+++ const.py
@@ -1,2 +1,12 @@
 CURRENCY_CHANNEL_ID = 1147252220417560588
 NEWS_CHANNEL_ID = 1153763737195532338
+INDEED_URL = "https://ca.indeed.com/jobs?q=Junior+Software+developer&l=Vancouver%2C+BC&radius=100&vjk=64f885a26a22a348"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Connection": "keep-alive",
+    "Accept-Language": "en-US,en;q=0.9,lt;q=0.8,et;q=0.7,de;q=0.6",
+}
+
+PROXY_URL='https://proxy.scrapeops.io/v1/'
